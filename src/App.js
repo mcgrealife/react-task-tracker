7fcd4b91c090be0b5c4f408e0831c0ebb808be78
Michael McGreal <mcgrealife@gmail.com>
2021-12-03 19:16:47 -0500

    wow jsx is nice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// can only return a single parent element (JSX expressions must have 1 parent element)
+// can have multiple elements, but must be inside of a single div or fragment <> (empty angle bracket)
 
+// can add expressions and variables inside of the func
+// conditionals as ternary operators
 function App() {
+
+  const name = 'Moog'
+  const x = true
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>Hello from React</h1>
+      <h2>hello again {name}</h2>
+      <h3>can also do math inside of curly brackts like 1 + 2 <code>{1 + 2}</code> and can even wrap that in code</h3>
+      <h3>woah. I can even use ternary operators <em>inside</em> of curly brackers, like this {x ? 'Yes' : 'No'} </h3>
     </div>
   );
 }
