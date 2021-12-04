19ef870bfed69cbc078e5bf6e4de013bf9d9ab99
Michael McGreal <mcgrealife@gmail.com>
2021-12-03 20:10:24 -0500

    ohh, I can achieve this with props "destructuring" if needed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,14 @@
-const Header = (props) => {
+ // ohh I can achieve this with props "destructuring"
+const Header = ({ title }) => {
     return (
         <header>
-            <h1>{props.title}</h1>
+            <h1>{title}</h1>
             
         </header>
     )
 }
 
-// hmm, but defining defaultProps outside of the arrow function is less elegant. If I have to write them anyway, why not write them in the function declaration.
+
 Header.defaultProps = {
     title: 'defaultProp defined below the arrow function is weird though'
 }
