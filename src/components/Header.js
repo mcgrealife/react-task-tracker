1e2b069e18cd42e474b35b8b2f19cdc14dcac473
Michael McGreal <mcgrealife@gmail.com>
2021-12-03 20:13:35 -0500

    Prop Types  seem unnecessarily complicated. Can TypeScript handle easier prop type definitions?

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,7 @@
- // ohh I can achieve this with props "destructuring"
+// prop types
+import PropTypes from 'prop-types'
+
+
 const Header = ({ title }) => {
     return (
         <header>
@@ -11,6 +14,11 @@ const Header = ({ title }) => {
 
 Header.defaultProps = {
     title: 'defaultProp defined below the arrow function is weird though'
+}
+
+// I can still pass a number in {}, and the propTypes will convert it to a string. The console will show a warning though. Seems unnecessarily complicated. Can TypeScript handle easier prop type definitions?
+Header.propTypes = {
+    title: PropTypes.string
 }
 
 export default Header
