21d6f54ace9773095e630cf7722fbf7307ff6ac5
Michael McGreal <mcgrealife@gmail.com>
2021-12-06 13:04:02 -0500

    add task , deletetask

--- src/App.js
+++ src/App.js
@@ -1,10 +1,58 @@
+import { useState } from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
+
 
 function App() {
+  const [tasks, setTasks] = useState(
+    [
+        {
+            id: 1,
+            text: 'meeting at school',
+            day: 'feb 6th at 1:30pm',
+            reminder: true
+        },
+        {
+            id: 2,
+            text: 'another task',
+            day: 'feb 7th at 2:30pm',
+            reminder: true
+        },
+        {
+            id: 3,
+            text: 'order food',
+            day: 'feb 6th at 5:30pm',
+            reminder: false
+        }
+    ]
+)
+
+    // delete task
+    // where did this id come from? it this part of the browser?
+    const deleteTask = (id) => {
+      setTasks(tasks.filter((task) =>
+        task.id !== id
+        ))
+    }
+
+    const addTask = (task) => {
+      const id = Math.floor(10000) + 1;
+      const newTask = {id, ...task}
+      setTasks([...tasks, newTask])
+    }
+
+    function showForm() {
+
+    }
+
   return (
     <div className='container'>
-      <Header />
-      
+      <Header onClick={showForm}/>   
+      <AddTask onAdd={addTask}/>
+      {tasks.length === 0 ?
+        <Tasks tasks={tasks} onDelete={deleteTask}/> : 
+        <p>No tasks!!!</p> }
       
     </div>
   )
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,7 @@
 import Button from './Button'
 
-// oh destructuring props is just more efficient? (rather than passing a bloated prop object)
+const Header = ({ title = 'defaultTitle', onClick }) => {
 
-// this is much better resource https://beta.reactjs.org/learn/passing-props-to-a-component
-// the DefaultProps and PropType thing seems unnecessary based on react docs. 
-// Their style is much more readable to me
-// OHH okay, and TypeScript inherently replaces react PropTypes. PropTypes are availabel for JS files. Nice feature, but I'd rather use TS.
-
-const Header = ({ title = 'defaultTitle' }) => {
-    
-    const onClick = () => {
-        console.log('Click')
-    }
 
     return (
         <header className='header'>
